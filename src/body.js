e5500fa1c3666ebd91d5722e64f745c149108e04
CloudMojos <joshuel.simbulan@gmail.com>
2023-02-08 13:28:59 +0800

    add files

---
+++ src/body.js
@@ -0,0 +1,11 @@
+import sidebar from './sidebar'
+import todo from './todo'
+import notes from './notes'
+
+function createBody() {
+    const body = document.createElement('div');
+    
+}
+
+const body = createBody()
+export default body
